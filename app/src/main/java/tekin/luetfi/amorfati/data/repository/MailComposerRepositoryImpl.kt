a3d61f91f34d52c8e874080f4962d0fad27a5900
Lütfi Tekin <lutfi-tekin@outlook.com>
2025-04-19 20:54:54 +0200

    added default recipient email for the copy

--- app/src/main/java/tekin/luetfi/amorfati/data/repository/MailComposerRepositoryImpl.kt
+++ app/src/main/java/tekin/luetfi/amorfati/data/repository/MailComposerRepositoryImpl.kt
@@ -17,16 +17,16 @@ class MailComposerRepositoryImpl @Inject constructor(private val api: SendGridAp
                 mapOf(
                     "to"                    to listOf(
                         mapOf("email" to recipientEmail),
-                        mapOf("email" to Defaults.recipient.name)
+                        mapOf("email" to Defaults.recipient.email)
                     ),
                     "dynamic_template_data" to dynamicData
                 )
             ),
-            "from"        to mapOf("email" to Defaults.sender.email),
+            "from"        to mapOf("email" to Defaults.sender.email, "name" to "Lütfi Tekin"),
             "template_id" to TEMPLATE_ID
         )
 
-        api.sendMail(envelope)
-
+        val response = api.sendMail(envelope)
+        if (response.isSuccessful.not()) error("Failed to send email")
     }
 }

38d2971e3bc25de36e604c2ead9e0e4f6fde9c34
Lütfi Tekin <lutfi-tekin@outlook.com>
2025-04-25 09:32:37 +0200

    Refactor: Remove unused template ID
    - Removed the unused constant `TEMPLATE_ID`.

--- app/src/main/java/tekin/luetfi/amorfati/utils/Constants.kt
+++ app/src/main/java/tekin/luetfi/amorfati/utils/Constants.kt
@@ -20,7 +20,6 @@ object Defaults{
 const val SEND_GRID_BASE_URL = "https://api.sendgrid.com/"
 const val LORE_API_BASE_URL = "https://lutfitek.in/"
 const val DEFAULT_TIMEOUT = "default_timeout"
-const val TEMPLATE_ID = "d-7690a1effdad4c6295cfe1d4c343987f"
 const val SEND_GRID_API_KEY = "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"
 const val IMAGE_HOST_DIR = "https://lutfitek.in/assets/"
 const val DEFAULT_BACK_IMAGE = IMAGE_HOST_DIR + "back.png"

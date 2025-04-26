fdd0a0120a7e0bb204d0f083f2e5b2246818fdd5
Lütfi Tekin <lutfi-tekin@outlook.com>
2025-04-26 11:47:20 +0200

    Refactor: Update LoreApi endpoint
    - Removed unnecessary comments.
    - Modified the endpoint description to be more concise.

--- app/src/main/java/tekin/luetfi/amorfati/data/remote/LoreApi.kt
+++ app/src/main/java/tekin/luetfi/amorfati/data/remote/LoreApi.kt
@@ -5,10 +5,7 @@ import tekin.luetfi.amorfati.domain.model.Lore
 
 
 interface LoreApi {
-    /**
-     * Assuming your Retrofit baseUrl is "https://lutfitek.in/"
-     * this will fetch https://lutfitek.in/assets/lore.json
-     */
+
     @GET("assets/lore.json")
     suspend fun fetchLore(): Lore
 }

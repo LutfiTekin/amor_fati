96d72c537cbff9b85aa2a3f0eebee466af7f2535
Lütfi Tekin <lutfi-tekin@outlook.com>
2025-04-27 21:14:48 +0200

    Initialize MapLibre SDK
    - Initialized the MapLibre SDK in `AmorFatiApplication`.
    - Set the tile server to `WellKnownTileServer.MapLibre`.

--- app/src/main/java/tekin/luetfi/amorfati/di/AmorFatiApplication.kt
+++ app/src/main/java/tekin/luetfi/amorfati/di/AmorFatiApplication.kt
@@ -2,6 +2,16 @@ package tekin.luetfi.amorfati.di
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
+import org.maplibre.android.MapLibre
+import org.maplibre.android.WellKnownTileServer
 
 @HiltAndroidApp
-class AmorFatiApplication: Application()
+class AmorFatiApplication: Application(){
+
+    override fun onCreate() {
+        super.onCreate()
+        MapLibre.getInstance(this, "", WellKnownTileServer.MapLibre)
+    }
+
+
+}
